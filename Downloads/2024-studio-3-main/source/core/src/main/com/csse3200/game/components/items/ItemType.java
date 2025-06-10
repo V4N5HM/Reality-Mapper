9f52e87200e8b77e03a404e407b5b2d1046ff922
V4N5HM <s4820705@student.uq.edu.au>
2025-06-10 18:49:30 +1000

    Added the game

---
+++ Downloads/2024-studio-3-main/source/core/src/main/com/csse3200/game/components/items/ItemType.java
@@ -0,0 +1,22 @@
+package com.csse3200.game.components.items;
+
+public enum ItemType {
+    FISH,
+    APPLE,
+    BANANA,
+    BEEF,
+    ACAI,
+    LETTUCE,
+    CUCUMBER,
+    TOMATO,
+    STRAWBERRY,
+    CHOCOLATE,
+    FRUITSALAD,
+    ACAIBOWL,
+    SALAD,
+    STEAKMEAL,
+    BANANASPLIT,
+    FIREEXTINGUISHER,
+    PLATE,
+    MEAL
+}
---
+++ Downloads/2024-studio-3-main/source/core/src/main/com/csse3200/game/events/listeners/LevelListeners.java
@@ -0,0 +1,7 @@
+package com.csse3200.game.events.listeners;
+
+
+public class LevelListeners {
+    public LevelListeners() {
+    }
+}
